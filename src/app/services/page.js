a9498cf54ba4ca47bf4d92a65579fd12495d0206
KIM628 <kareemesmael88@gmail.com>
2024-04-14 00:20:51 +0200

    0

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,65 +1,39 @@
-'use client';
-import { useState } from "react";
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+'use client'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function Services() {
-  const slides = [
-    {url:'/assets/img/Reagents/Reagents-101.jpg'},
-    {url:'/assets/img/Reagents/Reagents-102.jpg'},
-    {url:'/assets/img/Reagents/Reagents-103.jpg'},
-    {url:'/assets/img/Reagents/Reagents-104.jpg'},
-  ]
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-  const [currentIndex, seCurrentIndex] = useState(0);
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    seCurrentIndex(newIndex);
-  }
-  
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    seCurrentIndex(newIndex);
-  }
+// import required modules
+import { Navigation } from 'swiper/modules';
 
-  const goToSlide = (slideIndex) => {
-    seCurrentIndex(slideIndex)
-  }
-
+export default function App() {
   return (
-    <section className="section">
+    <section className='section'>
       <div className="max-w-[560px] h-[900px] w-full m-auto py-16 px-4 relative group">
-        <h4 className="h4 mb-5 text-center">Reagents</h4>
-        <div
-          style={{backgroundImage:`url(${slides[currentIndex].url})`}}
-          className="w-full h-full rounded-xl bg-center bg-cover duration-300">
-          {/* Left arrow  */}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-            <BsChevronCompactLeft
-              onClick={prevSlide}
-              size={20} />
-          </div>
-          {/* Right arrow  */}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-            <BsChevronCompactRight
-              onClick={nextSlide}
-              size={20} />
-          </div>
-        </div>
-          <div className="flex bottom-4 justify-center py-2">
-            {slides.map((slide, slideIndex) => (
-              <div 
-                key={slideIndex}
-                onClick={()=>goToSlide(slideIndex)}
-                className="text-2xl cursor-pointer">
-                <RxDotFilled/>
-              </div>
-            ))}
-          </div>
+        <h4 className="h4 mb-5 text-center">Ouer Services</h4>
+        <p>We Provide Maintanens and Reagents for : </p>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <img src="/assets/img/devices/ABX_Micros.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/devices/Dymind.jpeg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/devices/ERMA-PCE-210N.jpeg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/devices/Sysmex-KX21.jpeg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/devices/Sysmex-XS500i.jpg" />
+          </SwiperSlide>
+        </Swiper>
       </div>
-
     </section>
-  )
+  );
 }
--- src/componants/MobileNav.js
+++ src/componants/MobileNav.js
@@ -68,16 +68,6 @@ export default function MobileNav() {
             </Link>
           </li>
         </ul>
-        {/* form */}
-        {/* <form className="relative flex gap-x-[10px]">
-          <Search />
-          <input
-            type="text"
-            id="mnav-search-input"
-            placeholder="Search..."
-            className="outline-none w-[160px] border-b-2 focus:border-b-2 focus:border-accent placeholder:italic"
-          />
-        </form> */}
       </div>
     </nav>
   );
