ef140370c0fb9b17934fd8f41c96c6822ec18bed
KIM628 <kareemesmael88@gmail.com>
2024-04-15 05:49:30 +0200

    0

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -16,23 +16,26 @@ export default function App() {
       <div className="max-w-[560px] h-[900px] w-full m-auto py-16 px-4 relative group">
         <h4 className="h4 mb-5 text-center">Ouer Services</h4>
         <p className="mb-[42px] md:max-w-xl">We provide maintenance and reagents for : </p>
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          <SwiperSlide>
-            <img src="/assets/img/devices/ABX_Micros.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/assets/img/devices/Dymind.jpeg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/assets/img/devices/ERMA-PCE-210N.jpeg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/assets/img/devices/Sysmex-KX21.jpeg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="/assets/img/devices/Sysmex-XS500i.jpg" />
-          </SwiperSlide>
-        </Swiper>
+        <div className="blog__post max-w-[420px] shadow-custom2 rounded-[10px]
+        overflow-hidden cursor-pointer group">
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            <SwiperSlide>
+              <img src="/assets/img/devices/ABX_Micros.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/assets/img/devices/Dymind.jpeg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/assets/img/devices/ERMA-PCE-210N.jpeg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/assets/img/devices/Sysmex-KX21.jpeg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/assets/img/devices/Sysmex-XS500i.jpg" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </section>
   );
